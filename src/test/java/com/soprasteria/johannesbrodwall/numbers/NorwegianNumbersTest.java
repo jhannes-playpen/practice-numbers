7201097f5aa423e7cbdd4ea2b5c5a25df5304967
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-08-08 10:57:16 +0200

    first test

---
+++ src/test/java/com/soprasteria/johannesbrodwall/numbers/NorwegianNumbersTest.java
@@ -0,0 +1,16 @@
+package com.soprasteria.johannesbrodwall.numbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NorwegianNumbersTest {
+    @Test
+    void shouldReturnEnForOne() {
+        assertEquals("en", toNorwegian(1));
+    }
+
+    private String toNorwegian(int i) {
+        return null;
+    }
+}
