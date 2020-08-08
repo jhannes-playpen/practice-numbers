d8af222f22232e66527a7c30d4102dce1466f39a
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-08-08 11:14:52 +0200

    test for 155 grønn (extract class NorwegianNumbers)

--- src/main/java/com/soprasteria/johannesbrodwall/numbers/NorwegianNumbers.java
+++ src/main/java/com/soprasteria/johannesbrodwall/numbers/NorwegianNumbers.java
@@ -3,17 +3,19 @@ package com.soprasteria.johannesbrodwall.numbers;
 import java.util.Map;
 
 public class NorwegianNumbers {
+
+    private static Map<Integer, String> numbers = Map.of(1, "en",
+            2, "to",
+            3, "tre",
+            5, "fem",
+            9, "ni",
+            20, "tjue",
+            40, "førti",
+            50, "femti",
+            100, "ett hundre"
+    );
+
     public static String toNorwegian(int number) {
-        Map<Integer, String> numbers = Map.of(1, "en",
-                2, "to",
-                3, "tre",
-                5, "fem",
-                9, "ni",
-                20, "tjue",
-                40, "førti",
-                50, "femti",
-                100, "ett hundre"
-        );
         if (number > 100) {
             return toNorwegian(number - number % 100) + " og " + toNorwegian(number % 100);
         }
