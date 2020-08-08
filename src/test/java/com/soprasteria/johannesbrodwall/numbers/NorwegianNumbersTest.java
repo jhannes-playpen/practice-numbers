3bc7ea4984ddd31c048cefda66d2b91ee2742c76
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-08-08 11:03:38 +0200

    start test refactoring to dynamicTest

--- src/test/java/com/soprasteria/johannesbrodwall/numbers/NorwegianNumbersTest.java
+++ src/test/java/com/soprasteria/johannesbrodwall/numbers/NorwegianNumbersTest.java
@@ -1,12 +1,22 @@
 package com.soprasteria.johannesbrodwall.numbers;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NorwegianNumbersTest {
+    @TestFactory
+    List<DynamicTest> numberTests() {
+        return List.of(
+                DynamicTest.dynamicTest("return tjue for 20", () -> assertEquals("tjue", toNorwegian(20)))
+        );
+    }
+
     @Test
     void shouldReturnEnForOne() {
         assertEquals("en", toNorwegian(1));
@@ -23,7 +33,7 @@ public class NorwegianNumbersTest {
     }
 
     private String toNorwegian(int i) {
-        Map<Integer, String> numbers = Map.of(1, "en", 2, "to");
+        Map<Integer, String> numbers = Map.of(1, "en", 2, "to", 3, "tre", 20, "tjue");
         return numbers.get(i);
     }
 }
